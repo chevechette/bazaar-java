98d94f1c61f55df98f768e0e3cdb81fe430c32bc
tuonetar <karina.martynava@gmail.com>
2022-02-11 14:58:40 +0100

    Ex4

---
+++ JavaBasics/src/ajc/basics/City.java
@@ -0,0 +1,81 @@
+package ajc.basics;
+
+public class City {
+	private String		name;
+	private String		country;
+	private double		population;
+	private String		category;
+	private boolean		capital;
+	
+	public City(String name, String country, double population, boolean capital) {
+		this.setName(name);
+		this.setCountry(country);
+		this.setPopulation(population);
+		this.setCapital(capital);
+	}
+	
+	public String	makeCompare(City c) {
+		String		comparator;
+		
+		if (this.population > c.population) {
+			comparator = "bigger than";
+		} else if (this.population < c.population) {
+			comparator = "smaller than";
+		} else {
+			comparator = "equals to";
+		}
+		return(String.format("City %s is %s City %s", this.name, comparator, c.name));
+	}
+
+	private void		updateCategory() {
+		if (this.population < 6 || (this.capital && this.population <= 7)) {
+			this.category = "Small";
+		} else {
+			this.category = "Big";
+		}
+	}
+
+	@Override
+	public String		toString() {
+		return "City [name=" + name + ", country=" +
+				country + ", population=" + population +
+				", category=" + category + ", capital=" + capital + "]";
+	}
+
+	public String		getName() {
+		return name;
+	}
+
+	public String		getCountry() {
+		return country;
+	}
+
+	public double		getPopulation() {
+		return population;
+	}
+
+	public String		getCategory() {
+		return category;
+	}
+
+	public boolean		isCapital() {
+		return capital;
+	}
+
+	public void			setName(String name) {
+		this.name = name;
+	}
+
+	public void			setCountry(String country) {
+		this.country = country;
+	}
+
+	public void			setPopulation(double population) {
+		this.population = population;
+		this.updateCategory();
+	}
+
+	public void			setCapital(boolean capital) {
+		this.capital = capital;
+	}
+}
--- JavaBasics/src/exercices/Exercices.java
+++ JavaBasics/src/exercices/Exercices.java
@@ -2,8 +2,43 @@ package exercices;
 
 import ajc.basics.Car;
 import ajc.basics.House;
+import ajc.basics.City;
 
 public class Exercices {
+	public static void exercice1() {
+		Car		backToFuture;
+		Car		tardis;
+		
+		// Method 1 w/out constructor. Pathetic.
+		// This will ONLY work as long as those properties are public.
+		// And we're private people 'ere.
+		
+//		backToFuture.color = "grey";
+//		backToFuture.model = "Dolorean";
+//		backToFuture.year = 1985;
+		
+		// Method 2 w/ constructor
+		backToFuture = new Car("grey", "Dolorean", 1985);
+		tardis = new Car("blue", "Type 40 Battle TARDIS", 1968);
+		
+		backToFuture.displayInfos();
+		tardis.displayInfos();
+	}
+	
+	public static void exercice2() {
+		Car		unknown;
+		Car		french;
+		Car		corvette;
+		
+		unknown = new Car();
+		french = new Car("pink");
+		corvette = new Car("nuclear", "covette", 3000);
+		
+		unknown.displayInfos();
+		french.displayInfos();
+		corvette.displayInfos();
+	}
+	
 	public static void exercice3() {
 		// Exercice added some getters/setters
 		// As well as a new house class
@@ -25,43 +60,26 @@ public class Exercices {
 		System.out.print(babaYaga);
 	}
 	
-	public static void exercice2() {
-		Car		unknown;
-		Car		french;
-		Car		corvette;
+	public static void exercice4() {
+		City	chicago;
+		City	irkutsk;
+		City	shenzhen;
 		
-		unknown = new Car();
-		french = new Car("pink");
-		corvette = new Car("nuclear", "covette", 3000);
+		chicago = new City("Chicago", "U.S.A.", 2.746388, false);
+		irkutsk = new City("Irkutsk", "Russia", 0.617473, false);
+		shenzhen = new City("Shenzhen", "China", 7.494398, false);
 		
-		unknown.displayInfos();
-		french.displayInfos();
-		corvette.displayInfos();
-	}
-	
-	public static void exercice1() {
-		Car		backToFuture;
-		Car		tardis;
+		System.out.println(chicago.toString());
+		System.out.println(irkutsk.toString());
+		System.out.println(shenzhen.toString());
 		
-		// Method 1 w/out constructor. Pathetic.
-		// This will ONLY work as long as those properties are public.
-		// And we're private people 'ere.
-		
-//		backToFuture.color = "grey";
-//		backToFuture.model = "Dolorean";
-//		backToFuture.year = 1985;
-		
-		// Method 2 w/ constructor
-		backToFuture = new Car("grey", "Dolorean", 1985);
-		tardis = new Car("blue", "Type 40 Battle TARDIS", 1968);
-		
-		backToFuture.displayInfos();
-		tardis.displayInfos();
+		System.out.println(shenzhen.makeCompare(chicago));
 	}
 	
 	public static void main(String[] args) {
-		exercice1();
-		exercice2();
-		exercice3();
+//		exercice1();
+//		exercice2();
+//		exercice3();
+		exercice4();
 	}
 }
